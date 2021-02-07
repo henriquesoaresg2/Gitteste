0af2dbcae207be9ca2fcff8e70e88b7edf4bc41a
henriquesoaresg2 <henriquesoaresg2@hotmail.com>
2021-02-07 16:41:36 -0300

    Incrementado efeito label skin

--- DesviaTudo/HUD.gd
+++ DesviaTudo/HUD.gd
@@ -2,8 +2,11 @@ extends CanvasLayer
 
 signal start_game
 
+var tempo = Timer.new()
+
 func _ready():
-	pass # Replace with function body.
+	tempo.connect("timeout", self, "_on_tempo_timeout")
+	add_child(tempo)
 
 func show_message(text):
 	$Message.text = text
@@ -11,6 +14,7 @@ func show_message(text):
 	$MessageTimer.start()
 
 func show_game_over():
+	$Hints.hide()
 	show_message("Game Over")
 	yield($MessageTimer, "timeout") # Espera ate terminar o contador do MessageTimer.
 	$Message.text = "Desvia ou\nmorre!"
@@ -25,7 +29,20 @@ func update_score(score):
 func _on_StartButton_pressed():
 	$StartButton.hide()
 	emit_signal("start_game")
+	$Hints.show()
+	_brilha_Message()
 
 func _on_MessageTimer_timeout():
 	$Message.hide()
 	$Credits.hide()
+
+func _brilha_Message():
+	tempo.start(1.5)
+
+func _on_tempo_timeout():
+	if $Hints.is_visible():
+		$Hints.hide()
+	else:
+		$Hints.show()
+	#falta fazer um if para se mostrar o player, chamar funcao abaixo.
+	_brilha_Message()
